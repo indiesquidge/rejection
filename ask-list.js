242e116396079dd2cf594b47022db2f764d30eb3
Austin Wood <i@austinwood.me>
2016-12-17 17:33:27 -0800

    Update askList function for better readability

--- ask-list.js
+++ ask-list.js
@@ -1,78 +1,99 @@
 (() => {
   const newAskForm = document.querySelector('.ask-form')
-  const askEl = document.getElementById('ask')
-  const askeeEl = document.getElementById('askee')
+  const askInput = document.getElementById('ask')
+  const askeeInput = document.getElementById('askee')
   const acceptedButton = document.getElementById('accepted')
   const rejectedButton = document.getElementById('rejected')
-  let askListEl = document.querySelector('.ask-list')
-  let scoreEl = document.querySelector('.current-score')
+  const askListEl = document.querySelector('.ask-list')
+  const scoreEl = document.querySelector('.current-score')
 
-  let askList = (() => {
+  const myAskList = askList()
+  myAskList.renderListAndScore()
+
+  acceptedButton.addEventListener('click', (e) => {
+    if (askInput.validity.valid && askeeInput.validity.valid) {
+      e.preventDefault()
+      myAskList.newAsk('accepted')
+    }
+  })
+
+  rejectedButton.addEventListener('click', (e) => {
+    if (askInput.validity.valid && askeeInput.validity.valid) {
+      e.preventDefault()
+      myAskList.newAsk('rejected')
+    }
+  })
+
+  function askList() {
     let list = JSON.parse(localStorage.getItem('askList')) || []
 
     const saveList = () => {
       localStorage.setItem('askList', JSON.stringify(list))
     }
 
-    const getList = () => {
-      list.forEach(ask => appendNewAskToDOM(ask))
-    }
-
-    const appendNewAskToDOM = (ask) => {
-      let newListEl = document.createElement('li')
-      newListEl.innerHTML = JSON.stringify(ask, null, 2)
+    const renderListAndScore = () => {
+      const listElements = list.map(convertToElement).join('')
+      askListEl.innerHTML = listElements
+      renderScore()
       newAskForm.reset()
-      askListEl.appendChild(newListEl)
     }
 
-    const getScore = () => {
-      return list.reduce((total, ask) => {
-        if (ask.status === 'accepted') {
-          return total + 1
-        }
-        if (ask.status === 'rejected') {
-          return total + 10
-        }
+    const convertToElement = ask => {
+      return `<li class="ask">
+                <h4>${ask.status}</h4>
+                <p>${ask.ask}</p>
+                <p>${ask.askee}</p>
+                <p>${ask.timestamp}</p>
+              </li>`
+    }
+
+    const renderScore = () => {
+      const scoreTotal = list.reduce((total, ask) => {
+        if (ask.status === 'accepted') return total + 1
+        if (ask.status === 'rejected') return total + 10
       }, 0)
+
+      scoreEl.innerHTML = scoreTotal
     }
 
-    const setScore = () => {
-      scoreEl.innerHTML = getScore()
-    }
-
-    const newAsk = (status) => {
+    const newAsk = status => {
       const ask = {
-        timestamp: Date.now(),
-        ask: askEl.value,
-        askee: askeeEl.value,
+        timestamp: currentDateAndTime(),
+        ask: askInput.value,
+        askee: askeeInput.value,
         status
       }
 
       list.push(ask)
-      appendNewAskToDOM(ask)
-      setScore()
       saveList()
+      renderListAndScore()
 
       return ask
     }
 
+    // create timestamp
+    const currentDateAndTime = () => {
+      const newDate = new Date()
+      newDate.setTime(Date.now())
+      const date = newDate.toLocaleDateString()
+      const time = currentTime(newDate)
+      return `${time}, ${date}`
+    }
+
+    const currentTime = dateObj => {
+      return (
+        ((dateObj.getHours() < 10) ? '0' : '') +
+        dateObj.getHours() +
+        ':' +
+        ((dateObj.getMinutes() < 10) ? '0' : '') +
+        dateObj.getMinutes()
+      )
+    }
+
+    // public methods
     return {
       newAsk,
-      setScore,
-      getList
+      renderListAndScore
     }
-  })()
-
-  askList.getList()
-  askList.setScore()
-
-  acceptedButton.addEventListener('click', (e) => {
-    e.preventDefault()
-    askList.newAsk('accepted')
-  })
-
-  rejectedButton.addEventListener('click', (e) => {
-    e.preventDefault()
-    askList.newAsk('rejected')
-  })
+  }
 })()
